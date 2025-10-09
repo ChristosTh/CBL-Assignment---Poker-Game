8c4cdf77f2359f9c657b85687b25c540ece87a79
Leo Fischer <fischer.leo12@gmail.com>
2025-10-09 11:54:25 +0200

    Create Pot class, create Wallet class. Add constructors for both, create small and big blind variables (values of each TBD). Create raised method in Pot class to update the Pot. Create raise and allIn method in wallet.

--- Pot.java
+++ Pot.java
@@ -1,6 +1,22 @@
-public class Pot{
+/** Pot class to sum and keep track of all the bets. */
+public class Pot {
+    private int potTotal = 0; 
+    private int sBlind = 0; 
+    private int bBlind = 0; 
 
-    // this is an updated comment.
-    // testing push functionality
-    // testing push functionality v2
+    /** Pot constructor. */
+    public Pot() {
+        potTotal = 0; 
+
+        // give values later to sBlind & bBlind.
+        sBlind = 0; 
+        bBlind = 0; 
+    }
+
+    /** in main method check if raise is possible and then run Pot.raised */ 
+    int raised(int x) {
+        potTotal += x; 
+        return potTotal; 
+    }
+
 }
---
+++ Wallet.java
@@ -0,0 +1,21 @@
+/** Wallet class for user. */
+public class Wallet {
+    private int wallet = 0; 
+
+    public Wallet(int x) {
+        wallet = x; 
+    }
+
+    /** in main method check if raise is possible and then run Pot.raised */
+    boolean raise(int x) {
+        if (x < wallet) {
+            wallet -= x; 
+            return true; 
+        }
+        return false;
+    }
+
+    boolean allIn() {
+        return false;
+    }
+}
