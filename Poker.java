840d7f9cc1381f5043c463876e5de75812e9fc04
Leo Fischer <fischer.leo12@gmail.com>
2025-10-09 12:33:36 +0200

    end of session commit, adding all changes

--- Poker.java
+++ Poker.java
@@ -1,4 +1,9 @@
 /** Main class for poker. */
 public class Poker {
-    
+    Pot pot = new Pot(); 
+    Player player; 
+    public static void main(String[] args) {
+        // console asks how much money
+
+    }
 }
