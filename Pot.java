04136da847313f8cc5630868c58c61c0b42d5b8c
Leo Fischer <fischer.leo12@gmail.com>
2025-10-10 18:05:20 +0200

    Made Wallet class inherit Pot class for better functionality and clearer code. Finished raise and call methods.

--- Pot.java
+++ Pot.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 /** Pot class to sum and keep track of all the bets. */
 public class Pot {
     private int potTotal = 0; 
     private int sBlind = 0; 
     private int bBlind = 0; 
     private int currentRaise = 0; 
+    ArrayList<Wallet> bets = new ArrayList<>(); // ArrayList to keep track of the user's last bet. 
 
     /** Pot constructor. */
     public Pot() {
@@ -15,16 +18,27 @@ public class Pot {
     }
 
     /** in main method check if raise is possible and then run Pot.raised */ 
-    int raised(int x) {
+    int potRaise(int x) {
         potTotal += x; 
-        currentRaise += x; 
+        currentRaise = x; 
         return potTotal; 
     }
 
     /** Call method for pot. */
-    int call(int x) {
+    int potCall(int x) {
         potTotal += x; 
         return potTotal; 
     }
 
+    /** Method to add small blind to the pot. */
+    int smallBlind(int x) {
+        potTotal += x; 
+        return potTotal;
+    }
+
+    /** Method to add big blind to the pot. */
+    int bigBlind(int x) {
+        potTotal += x; 
+        return potTotal; 
+    }
 }
--- Wallet.java
+++ Wallet.java
@@ -1,24 +1,31 @@
 /** Wallet class for user and bots. */
-public class Wallet {
+public class Wallet extends Pot {
     private int wallet = 0; 
+    private int lastBet = 0; 
 
     public Wallet(int x) {
         wallet = x; 
+        lastBet = 0; 
     }
 
     /** in main method check if raise is possible and then run Pot.raised */
-    boolean raise(int x) {
+    boolean actionRaise(int x) {
         if (x < wallet) {
             wallet -= x; 
+            lastBet = x; 
+            potRaise(x); 
             return true; 
         }
         return false;
     }
 
     /** Method to call on a raise. */
-    boolean call(int currentRaise) {
-        if (currentRaise < wallet) {
-            wallet -= currentRaise; 
+    boolean actionCall(int currentRaise) {
+        int betDifference = currentRaise - lastBet; 
+        if (betDifference < wallet) {
+            wallet -= betDifference; 
+            lastBet += betDifference; 
+            potCall(betDifference); 
             return true; 
         }
         return false; 
