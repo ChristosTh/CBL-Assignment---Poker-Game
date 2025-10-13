bbaf3059382cf9cd102de86b4b174fd6f429a6ca
Leo Fischer <fischer.leo12@gmail.com>
2025-10-13 16:10:53 +0200

    removing stuff from comments

--- Poker.java
+++ Poker.java
@@ -16,30 +16,34 @@ public class Poker {
         // console asks how much money
 
         deck.shuffleCards();
-        //Card card = deck.giveCard();
-        //card.displayCard();
+        Card card = deck.giveCard();
+        card.displayCard();
 
         
-        /*System.out.println("Please enter three integers separated by a single space: "
+        System.out.println("Please enter three integers separated by a single space: "
                 + "How much money you'd like to play with, the small blind, and the big blind.");
         Scanner input = new Scanner(System.in);
         int moneyAmount = input.nextInt(); 
         int small = input.nextInt();
         int big = input.nextInt(); 
         player = new Player(moneyAmount, small, big);
-        input.close(); */
+        input.close(); 
 
         player.receiveFirstCard(deck.giveCard());
         player.receiveSecondCard(deck.giveCard());
 
-        System.out.println("Card 1: " + player.getFirstCard().getRank() + " " + player.getFirstCard().getSuit());
-        System.out.println("Card 2: " + player.getSecondCard().getRank() + " " + player.getSecondCard().getSuit());
+        System.out.println("Card 1: " 
+            + player.getFirstCard().getRank() + " " + player.getFirstCard().getSuit());
+        System.out.println("Card 2: " 
+            + player.getSecondCard().getRank() + " " + player.getSecondCard().getSuit());
 
         bot.receiveFirstCard(deck.giveCard());
         bot.receiveSecondCard(deck.giveCard());
 
-        System.out.println("Bot Card 1: " + bot.getFirstCard().getRank() + " " + bot.getFirstCard().getSuit());
-        System.out.println("Bot Card 2: " + bot.getSecondCard().getRank() + " " + bot.getSecondCard().getSuit());
+        System.out.println("Bot Card 1: " 
+            + bot.getFirstCard().getRank() + " " + bot.getFirstCard().getSuit());
+        System.out.println("Bot Card 2: " 
+            + bot.getSecondCard().getRank() + " " + bot.getSecondCard().getSuit());
 
     }
 }
