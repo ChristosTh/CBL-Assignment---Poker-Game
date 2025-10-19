5f5b6626d3ef96cfb788a70782b4ec2f47434d89
Leo Fischer <fischer.leo12@gmail.com>
2025-10-19 23:41:55 +0200

    UI changes: new wallet display which updates in real time.

--- PokerMat.java
+++ PokerMat.java
@@ -3,22 +3,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 import javax.swing.*;
 
 
 /** Javadoc. */
 public class PokerMat {
     JFrame pokerMat; 
-
-    JLabel walletDisplay; 
-    JLabel bBlindDisplay; 
-    JLabel sBlindDisplay; 
-
-    ImageIcon faceDownCard = new ImageIcon("CardsPNG\\faceDownCard.png"); 
-    Image scalingImage = faceDownCard.getImage().getScaledInstance(157, 160, Image.SCALE_DEFAULT);
-    ImageIcon scaledImage = new ImageIcon(scalingImage); 
-    JLabel imageContainer = new JLabel(scaledImage);  
 
     static ImageIcon playerCard1; 
     static ImageIcon playerCard2; 
@@ -35,6 +25,22 @@ public class PokerMat {
         frame = null; 
     }
 
+    void updateWalletDisplay(JLabel label) {
+        String newWallet = Double.toString(Poker.player.getWallet()); 
+        if (newWallet.substring(newWallet.length() - 2).equals(".0")) {
+            newWallet = newWallet.substring(0, newWallet.length() - 2);
+        }
+        newWallet = "<html><span style='font-size:10.3px'>" 
+            + newWallet + "</span></html>";
+        label.setText(newWallet); 
+    }
+
+    //#region UI components
+    ImageIcon faceDownCard = new ImageIcon("CardsPNG\\faceDownCard.png"); 
+    Image scalingImage = faceDownCard.getImage().getScaledInstance(157, 160, Image.SCALE_DEFAULT);
+    ImageIcon scaledImage = new ImageIcon(scalingImage); 
+    JLabel imageContainer = new JLabel(scaledImage);  
+
     Image scalingCard1 = playerCard1.getImage().getScaledInstance(115, 160, Image.SCALE_DEFAULT); 
     ImageIcon scaledCard1 = new ImageIcon(scalingCard1); 
     JLabel card1Container = new JLabel(scaledCard1); 
@@ -43,30 +49,39 @@ public class PokerMat {
     ImageIcon scaledCard2 = new ImageIcon(scalingCard2); 
     JLabel card2Container = new JLabel(scaledCard2); 
 
+    ImageIcon chipStacks = new ImageIcon("CardsPNG\\Chip_Stacks.png"); 
+    Image scalingChips = chipStacks.getImage().getScaledInstance(46, 46, Image.SCALE_DEFAULT); 
+    ImageIcon scaledChips = new ImageIcon(scalingChips); 
+    JLabel chipContainer = new JLabel(scaledChips); 
+
+    JLabel moneyDisplay; 
+    ImageIcon moneyBox = new ImageIcon("CardsPNG\\Money_Box.png"); 
+    Image scalingBox = moneyBox.getImage().getScaledInstance(130, 60, Image.SCALE_DEFAULT);
+    ImageIcon scaledBox = new ImageIcon(scalingBox); 
+    JLabel boxContainer = new JLabel(scaledBox);
+
     JButton raiseButton; 
     JButton callButton; 
     JButton foldButton; 
     JButton allInButton; 
     JButton checkButton; 
+    //#endregion
 
     /** PokerMat page constructor. */
     public PokerMat(double moneyAmount, double smallBlind, double bigBlind) {
         Poker.poker.setBlinds(smallBlind, bigBlind); 
         Poker.player.setWallet(moneyAmount); 
 
+        //#region Poker mat Frame creation/customisation
         pokerMat = new JFrame("Poker"); 
         pokerMat.setSize(1100, 750); 
         pokerMat.setVisible(true); 
         pokerMat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pokerMat.getContentPane().setBackground(new Color(6, 117, 8));
         pokerMat.setLocationRelativeTo(null);
+        //#endregion
 
-        /* display small blind:
-        sBlindDisplay = new JLabel(Double.toString(Poker.poker.pot.getSmallBlind())); 
-        sBlindDisplay.setSize(200, 200); 
-        sBlindDisplay.setLocation(400, 400); */
-
-        //#region Displaying cards.
+        //#region Displaying cards and chips/wallet.
         imageContainer.setSize(180, 180); 
         imageContainer.setLocation(100, 250); 
 
@@ -75,6 +90,23 @@ public class PokerMat {
 
         card2Container.setSize(115, 160); 
         card2Container.setLocation(550, 500);
+
+        chipContainer.setSize(46, 46); 
+        chipContainer.setLocation(903, 25); 
+
+        boxContainer.setSize(130, 60); 
+        boxContainer.setLocation(947, 20); 
+        
+        String initialWallet = Double.toString(Poker.player.getWallet()); 
+        if (initialWallet.substring(initialWallet.length() - 2).equals(".0")) {
+            initialWallet = initialWallet.substring(0, initialWallet.length() - 2);
+        }
+        initialWallet = "<html><span style='font-size:10.3px'>" 
+            + initialWallet + "</span></html>"; 
+        moneyDisplay = new JLabel(initialWallet);
+        moneyDisplay.setForeground(new Color(230, 165, 90));
+        moneyDisplay.setLocation(995, 33);  
+        moneyDisplay.setSize(100, 30); 
         //#endregion
 
         //#region Raise functionality.
@@ -105,7 +137,7 @@ public class PokerMat {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         Poker.player.raise(Double.parseDouble(raiseAmount.getText())); 
-                        System.out.println("wtf" + Poker.pot.getCurrentRaise()); 
+                        updateWalletDisplay(moneyDisplay);
                         closeFrame(raiseFrame); 
                     }
                 });
@@ -135,6 +167,8 @@ public class PokerMat {
             @Override 
             public void mouseClicked(MouseEvent e) {
                 Poker.player.call(Poker.pot.getCurrentRaise()); 
+                updateWalletDisplay(moneyDisplay);
+
             }
         });
         //#endregion
@@ -147,6 +181,7 @@ public class PokerMat {
             @Override 
             public void actionPerformed(ActionEvent e) {
                 Poker.player.allIn(); 
+                updateWalletDisplay(moneyDisplay);
             }
         }); 
         //#endregion
@@ -187,6 +222,10 @@ public class PokerMat {
         pokerMat.add(callButton); 
         pokerMat.add(foldButton); 
         pokerMat.add(allInButton); 
+        pokerMat.add(checkButton); 
+        pokerMat.add(chipContainer); 
+        pokerMat.add(moneyDisplay); 
+        pokerMat.add(boxContainer); 
         pokerMat.setLayout(new BorderLayout());
     }
 }
